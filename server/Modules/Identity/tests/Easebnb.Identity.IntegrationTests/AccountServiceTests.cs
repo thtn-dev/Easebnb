a0673e9fe382b50260e53350b37c0ef31f6cb525
thtn-dev <thtn.1611.dev@gmail.com>
2026-08-19 23:00:20 +0700

    feat: add integration tests for account service and update project structure

---
+++ server/Modules/Identity/tests/Easebnb.Identity.IntegrationTests/AccountServiceTests.cs
@@ -0,0 +1,23 @@
+﻿using Easebnb.Identity.Core.Dtos;
+using Easebnb.Identity.Core.Entities;
+using FluentAssertions;
+
+namespace Easebnb.Identity.IntegrationTests;
+
+public class AccountServiceTests : IdentityModuleTestBase
+{
+    public AccountServiceTests(IdentityModuleFixture fixture) : base(fixture) { }
+
+    [Fact]
+    public async Task ChangePassword_WithCorrectCurrentPassword_Succeeds()
+    {
+        var user = new User { UserName = "test@easebnb.com", Email = "test@easebnb.com" };
+        await UserManager.CreateAsync(user, "OldPass123!");
+
+        var request = new ChangePasswordRequest("OldPass123!", "NewPass123!", "NewPass123!");
+
+        var result = await AccountService.ChangePasswordAsync(user.Id, request);
+
+        result.IsError.Should().BeFalse();
+    }
+}
